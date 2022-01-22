5b0ff99fdabf97d69dd1ca81a01bab2bbba14952
NVKhanh <nvk020697@gmail.com>
2022-01-22 21:19:19 +0700

    update login form, group form

--- src/GroupForm/index.js
+++ src/GroupForm/index.js
@@ -83,93 +83,99 @@ function GroupForm() {
     }
   })
   return (
-    <div className="login_form">
+    <div className="add_form">
       <div className="krqetT"></div>
       <div className="ifKAln"></div>
       <LogOut />
       <Container>
-        <Row>
-          <Col md={2}></Col>
-          <Col md={8}>
-            <form onSubmit={formik.handleSubmit}>
-              <div className="login_wrapper">
-                <div className="formsix-pos">
-                  <div className="form-group">
-                    <InputForm
-                      type="text"
-                      id="Text1"
-                      placeholder="Tiêu đề *"
-                      name="label"
-                      defaultValue={formik.values.label}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {formik.errors.label && formik.touched.label && <p className="msg_err">{formik.errors.label}</p>}
+        <div className="form_bg">
+          <Row>
+            <Col lg={7} className="div_bg">
+              <img
+                src={'http://moitruongdulich.vn/mypicture/images/2020/CNMN42020/185ISO-21401.jpg'}
+                className="img_bg"
+              />
+            </Col>
+            <Col lg={5} className="form_left">
+              <form onSubmit={formik.handleSubmit}>
+                <div className="login_wrapper">
+                  <div className="formsix-pos">
+                    <div className="form-group">
+                      <InputForm
+                        type="text"
+                        id="Text1"
+                        placeholder="Tiêu đề *"
+                        name="label"
+                        defaultValue={formik.values.label}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />
+                      {formik.errors.label && formik.touched.label && <p className="msg_err">{formik.errors.label}</p>}
+                    </div>
                   </div>
-                </div>
-                <div className="formsix-e">
-                  <div className="form-group i-password">
-                    <InputForm
-                      type="text"
-                      id="Text2"
-                      placeholder="Nội dung *"
-                      name="content"
-                      defaultValue={formik.values.content}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {formik.errors.content && formik.touched.content && (
-                      <p className="msg_err">{formik.errors.content}</p>
-                    )}
+                  <div className="formsix-e">
+                    <div className="form-group i-password">
+                      <InputForm
+                        type="text"
+                        id="Text2"
+                        placeholder="Nội dung *"
+                        name="content"
+                        defaultValue={formik.values.content}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />
+                      {formik.errors.content && formik.touched.content && (
+                        <p className="msg_err">{formik.errors.content}</p>
+                      )}
+                    </div>
                   </div>
-                </div>
 
-                <div className="login_btn_wrapper" style={{ textAlign: 'left' }}>
-                  <a href="#" className="btn btn-primary" onClick={() => setShows(true)}>
-                    Thêm địa điểm
-                  </a>
-                  <ModalForm
-                    show={shows}
-                    onHide={() => setShows(false)}
-                    ModalTile={''}
-                    ModalChildren={<MapboxLocationVote onClose={onClose} />}
-                    size="xl"
-                  />
-                </div>
+                  <div className="login_btn_wrapper" style={{ textAlign: 'left' }}>
+                    <a href="#" className="btn btn-primary" onClick={() => setShows(true)}>
+                      Thêm địa điểm
+                    </a>
+                    <ModalForm
+                      show={shows}
+                      onHide={() => setShows(false)}
+                      ModalTile={''}
+                      ModalChildren={<MapboxLocationVote onClose={onClose} />}
+                      size="xl"
+                    />
+                  </div>
 
-                <div className="address_vote">
-                  {locationVote.map(value => (
-                    <button type="button" key={`${value} +1`} className="btn_address" onClick={() => setShow(true)}>
-                      {value}
+                  <div className="address_vote">
+                    {locationVote.map(value => (
+                      <button type="button" key={`${value} +1`} className="btn_address" onClick={() => setShow(true)}>
+                        {value}
+                      </button>
+                    ))}
+
+                    <ModalForm
+                      show={show}
+                      onHide={() => setShow(false)}
+                      ModalTile={''}
+                      ModalChildren={<Mapbox onClose={onClose} />}
+                      size="xl"
+                    />
+                  </div>
+
+                  <div className="login_btn_wrapper" style={{ marginTop: '50px' }}>
+                    <button type="submit" onClick={e => handleGoBack(e)} className="btn login_btn">
+                      Trở Về
                     </button>
-                  ))}
-
-                  <ModalForm
-                    show={show}
-                    onHide={() => setShow(false)}
-                    ModalTile={''}
-                    ModalChildren={<Mapbox onClose={onClose} />}
-                    size="xl"
-                  />
+                    <button
+                      type="submit"
+                      className="btn login_btn"
+                      disabled={!(formik.isValid && formik.dirty && locationVote.length != 0)}
+                    >
+                      TẠO PHÒNG BÌNH CHỌN
+                    </button>
+                  </div>
                 </div>
-
-                <div className="login_btn_wrapper" style={{ marginTop: '50px' }}>
-                  <button type="submit" onClick={e => handleGoBack(e)} className="btn login_btn">
-                    Trở Về
-                  </button>
-                  <button
-                    type="submit"
-                    className="btn login_btn"
-                    disabled={!(formik.isValid && formik.dirty && locationVote.length != 0)}
-                  >
-                    TẠO PHÒNG BÌNH CHỌN
-                  </button>
-                </div>
-              </div>
-            </form>
-          </Col>
-          <Col md={2}></Col>
-        </Row>
+              </form>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   )
